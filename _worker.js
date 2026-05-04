da96155a3180b9b7d92b74eaae03c14ea59cf3bf
ercanalayli <ercanalayli@gmail.com>
2026-05-04 22:15:36 +0300

    Attach migration precheck dashboard

--- _worker.js
+++ _worker.js
@@ -9,6 +9,7 @@ export default {
     const files = ['alkam-reliability-guard-v1.js','alkam-cari-core-v4.js','alkam-last-amounts-v4g.js','alkam-data-quality-v1.js','alkam-tahakkuk-v5.js','alkam-tahakkuk-control-v5c.js','alkam-balance-highlight-v1.js','alkam-professional-ui-v1.js','alkam-finans-flow-v6.js','alkam-finans-ui-v6.js','alkam-tahsilat-v7.js','alkam-tahsilat-ui-v7.js','alkam-moka-ui-v6.js','alkam-banka-onay-v8.js','alkam-banka-onay-ui-v8.js','alkam-banka-import-ui-v8.js','alkam-banka-file-import-v8.js','alkam-banka-import-guard-v8.js','alkam-banka-import-errors-v8.js','alkam-banka-template-v8.js','alkam-banka-history-v8.js','alkam-banka-history-export-v8.js','alkam-guvenilirlik-raporu-v9.js','alkam-guvenilirlik-export-v9.js','alkam-kritik-uyari-v9.js','alkam-saglik-kontrol-v9.js','alkam-gunluk-kontrol-v9.js','alkam-tek-kontrol-rozeti-v9.js','alkam-gunluk-kontrol-export-v9.js','alkam-modul-registry-v10.js','alkam-system-manifest-v10.js'];
     let scripts = files.map((src)=>'<script src="/'+src+'"></script>').join('');
     scripts += '<script src="/alkam-loader-kontrol-v10.js"></script>';
+    scripts += '<script src="/alkam-migration-precheck-v10.js"></script>';
     html = html.replace('</head>', hide + scripts + '</head>');
 
     const headers = new Headers(response.headers);
