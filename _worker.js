93c8e91ff4c5fe936a1ed4bd40d2ba1a0f55d1a8
ercanalayli <ercanalayli@gmail.com>
2026-05-04 22:40:04 +0300

    Attach Supabase write gate blocker

--- _worker.js
+++ _worker.js
@@ -15,6 +15,7 @@ export default {
     scripts += '<script src="/alkam-supabase-baglanti-test-v10.js"></script>';
     scripts += '<script src="/alkam-supabase-readonly-v10.js"></script>';
     scripts += '<script src="/alkam-supabase-compare-ui-v10.js"></script>';
+    scripts += '<script src="/alkam-supabase-write-gate-v10.js"></script>';
     html = html.replace('</head>', hide + scripts + '</head>');
 
     const headers = new Headers(response.headers);
