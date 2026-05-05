b56a1624171b7f8fb2e5c31968500744659388da
ercanalayli <ercanalayli@gmail.com>
2026-05-05 14:35:15 +0300

    Load stable UI standard cleanup

--- _worker.js
+++ _worker.js
@@ -26,6 +26,7 @@ export default {
     scripts += '<script src="/alkam-ai-asistan-merkezi-v11.js"></script>';
     scripts += '<script src="/alkam-actionbar-sade-v11.js"></script>';
     scripts += '<script src="/alkam-dashboard-kurumsal-v11.js"></script>';
+    scripts += '<script src="/alkam-ui-standart-v1.js"></script>';
     html = html.replace('</head>', hide + scripts + '</head>');
 
     const headers = new Headers(response.headers);
