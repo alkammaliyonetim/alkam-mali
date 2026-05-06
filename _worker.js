bde29fcce75cdd7eedab0de750ce1cf0bc592034
ercanalayli <ercanalayli@gmail.com>
2026-05-06 09:51:08 +0300

    Load professional creative dashboard polish

--- _worker.js
+++ _worker.js
@@ -46,6 +46,7 @@ export default {
     scripts += '<script src="/alkam-merkezi-veri-uyari-v1.js"></script>';
     scripts += '<script src="/alkam-yedekleme-gecis-v1.js"></script>';
     scripts += '<script src="/alkam-gece-final-kontrol-v1.js"></script>';
+    scripts += '<script src="/alkam-profesyonel-ana-ekran-v1.js"></script>';
     html = html.replace('</head>', hide + scripts + '</head>');
 
     const headers = new Headers(response.headers);
