ea7e4612c0887c4fe00e96a03ac3193254509212
ercanalayli <ercanalayli@gmail.com>
2026-05-15 10:14:29 +0300

    Onay merkezi banka bekleyenler modulunu bagla

--- _worker.js
+++ _worker.js
@@ -6,9 +6,13 @@ export default {
     if (!contentType.includes('text/html')) return response;
 
     let html = await response.text();
-    const bankImport = '<script src="/alkam-bank-import-v1.js?v=bank-import-20260515a"></script>';
-    if (!html.includes('alkam-bank-import-v1.js')) {
-      html = html.replace('</body>', bankImport + '</body>');
+    const scripts = [
+      '<script src="/alkam-bank-import-v1.js?v=bank-import-20260515a"></script>',
+      '<script src="/alkam-bank-onay-center-v1.js?v=bank-onay-20260515a"></script>'
+    ];
+    for (const script of scripts) {
+      const marker = script.match(/\/([^\/\?]+)\?/)[1];
+      if (!html.includes(marker)) html = html.replace('</body>', script + '</body>');
     }
 
     headers.set('content-type', 'text/html; charset=utf-8');
